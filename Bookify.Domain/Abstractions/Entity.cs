0177f9e1f067e25dfc8f2240a546ec87d05534f0
Playground <Playground@LeonardosLaptop.home>
2023-10-22 22:53:13 +0200

    Apartment, Users, Booking and Share entities added

---
+++ Bookify.Domain/Abstractions/Entity.cs
@@ -0,0 +1,26 @@
+namespace Bookify.Domain.Abstractions;
+
+public abstract class Entity
+{
+   private readonly List<IDomainEvent> _domainEvents = new();
+   protected Entity(Guid id)
+   {
+      Id = id;
+   }
+   protected Guid Id { get; init; }
+
+   public IReadOnlyList<IDomainEvent> GetDomainEvents()
+   {
+      return _domainEvents.ToList();
+   }
+
+   public void ClearDomainEvents()
+   {
+      _domainEvents.Clear();
+   }
+
+   protected void RaiseDomainEvent(IDomainEvent domainEvent)
+   {
+      _domainEvents.Add(domainEvent);
+   }
+}
